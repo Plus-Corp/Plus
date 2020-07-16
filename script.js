8e2b109b6eb58a3f999059c0aaee83d7289e4fb4
Estorc <51833938+Estorc@users.noreply.github.com>
2020-07-16 19:22:35 +0200

    f
    f

--- script.js
+++ script.js
@@ -10,3 +10,56 @@ function parallax() {
 window.addEventListener("scroll", function(){
 	parallax();	
 });
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+$('body').addClass('stop-scrolling')
+$('.stop-clicking').css('visibility','visible');
+$('.stop-clicking').css('opacity','1');
+$('.stop-clicking').css('background-color','hsl(0, 0%, 0%, 50%)');
+$('.radiopopup').css('visibility','visible');
+$('.radiopopup').css('opacity','1');
+$('.radiopopup').css('background-color','hsl(0, 0%, 0%, 50%)');
+}
+
+// call this to Enable
+function enableScroll() {
+$('body').removeClass('stop-scrolling')
+$('.stop-clicking').css('visibility','hidden');
+$('.stop-clicking').css('opacity','0');
+$('.stop-clicking').css('background-color','hsl(0, 0%, 0%, 0%)');
+$('.radiopopup').css('visibility','hidden');
+$('.radiopopup').css('opacity','0');
+$('.radiopopup').css('background-color','hsl(0, 0%, 0%, 0%)');
+}
+ var tempDiv = $('#tempDiv');
+ tempDiv.load("http://109.169.15.20:17000/currentsong?sid=1.txt");
+ var html = tempDiv.html();
+ $('#radiotitle').html(html);
+
